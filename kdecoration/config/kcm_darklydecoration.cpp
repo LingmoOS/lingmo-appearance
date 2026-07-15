6b91c9db40f02969631e0cb4111bfd2380275397
Lingmo <lingmo@lingmo.org>
2026-07-15 18:05:03 +0800

    更新 CMake 配置以添加 clang-format 目标，并修正插件 JSON 文件名

--- kdecoration/config/kcm_darklydecoration.cpp
+++ kdecoration/config/kcm_darklydecoration.cpp
@@ -1,6 +1,6 @@
 #include "darklyconfigwidget.h"
 #include <KPluginFactory>
 
-K_PLUGIN_CLASS_WITH_JSON(Darkly::ConfigWidget, "kcm_darklydecoration.json")
+K_PLUGIN_CLASS_WITH_JSON(Darkly::ConfigWidget, "kcm_lingmoappearancedecoration.json")
 
 #include "kcm_darklydecoration.moc"
